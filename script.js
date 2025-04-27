0752cc5207fb828abb7c260587531ce178294ff1
Madhan-1306 <madhanraj13062005@gmail.com>
2025-04-27 15:16:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector(".menu-toggle").addEventListener("click", function() {
+    document.querySelector("nav ul").classList.toggle("active");
+});
+
+const countDownDate = new Date("May 7, 2025 00:00:00").getTime();
+
+const timer = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+    if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById("timer").innerHTML = "<p>Event Started!</p>";
+    }
+}, 1000);
